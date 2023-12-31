daac1f43f2f9666c8af0cdd72b39c6756fbb77ff
danilbushkov <danil1357997@gmail.com>
2023-12-31 19:16:17 +0300

    replace: Error -> Fatal

--- context/pathunit_test.go
+++ context/pathunit_test.go
@@ -41,158 +41,158 @@ var testJson = `
 func TestGetNameFromContext(t *testing.T) {
 	var context, err = NewContextFromJSON([]byte(testJson))
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	value, err := context.GetValue("users[0].name")
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	name, err := value.ToString()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	if name != "Alex" {
-		t.Errorf("The name is wrong")
+		t.Fatalf("The name is wrong")
 	}
 }
 
 func TestGetPhoneNumberFromContext(t *testing.T) {
 	var context, err = NewContextFromJSON([]byte(testJson))
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	value, err := context.GetValue("users[0].phoneNumbers[1]")
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	phone, err := value.ToString()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	if phone != "22222222" {
-		t.Errorf("The phone is wrong. Value: %s", phone)
+		t.Fatalf("The phone is wrong. Value: %s", phone)
 	}
 }
 
 func TestGetAgeFromContext(t *testing.T) {
 	var context, err = NewContextFromJSON([]byte(testJson))
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	value, err := context.GetValue("users[1].age")
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	age, err := value.ToInt()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	if int(age) != 24 {
-		t.Errorf("The phone is wrong")
+		t.Fatalf("The phone is wrong")
 	}
 }
 
 func TestValueFromContext(t *testing.T) {
 	var context, err = NewContextFromJSON([]byte(testJson))
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	value, err := context.GetValue("value")
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	v, err := value.ToInt()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	if int(v) != 10 {
-		t.Errorf("Value is wrong")
+		t.Fatalf("Value is wrong")
 	}
 }
 
 func TestArrayValueFromContext(t *testing.T) {
 	var context, err = NewContextFromJSON([]byte(testJson))
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	value, err := context.GetValue("array[2]")
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	v, err := value.ToString()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	if v != "c" {
-		t.Errorf("Value is wrong")
+		t.Fatalf("Value is wrong")
 	}
 }
 
 func TestImplicitAccessToArrayElement(t *testing.T) {
 	var context, err = NewContextFromJSON([]byte(testJson))
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	value, err := context.GetValue("array[arrayIndex]")
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	v, err := value.ToString()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	if v != "b" {
-		t.Errorf("Value is wrong")
+		t.Fatalf("Value is wrong")
 	}
 }
 func TestImplicitlyGetPhoneNumberFromContext(t *testing.T) {
 	var context, err = NewContextFromJSON([]byte(testJson))
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	value, err := context.GetValue("users[indices.users[1]].phoneNumbers[indices.phoneNumber]")
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	phone, err := value.ToString()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	if phone != "11111111" {
-		t.Errorf("The phone is wrong. Value: %s", phone)
+		t.Fatalf("The phone is wrong. Value: %s", phone)
 	}
 }
 func TestGetUserAgeFromContextUsingImplicitPropery(t *testing.T) {
 	var context, err = NewContextFromJSON([]byte(testJson))
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	value, err := context.GetValue("[property][0].age")
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	age, err := value.ToInt()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	if age != 20 {
-		t.Errorf("The phone is wrong. Value: %d", age)
+		t.Fatalf("The phone is wrong. Value: %d", age)
 	}
 }
 func TestGetPhoneNumberFromContextUsingImplicitPropery(t *testing.T) {
 	var context, err = NewContextFromJSON([]byte(testJson))
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	value, err := context.GetValue("[property][indices.[property][1]].phoneNumbers[indices.[property2]]")
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	phone, err := value.ToString()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	if phone != "11111111" {
-		t.Errorf("The phone is wrong. Value: %s", phone)
+		t.Fatalf("The phone is wrong. Value: %s", phone)
 	}
 }
